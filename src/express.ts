1815bd79310996f32b03d50eea181f28b4aedc9d
Zora Péteri <zorapeteri@gmail.com>
2023-06-30 18:58:28 +0200

    add cors to express

--- src/express.ts
+++ src/express.ts
@@ -1,7 +1,10 @@
 import express from 'express'
+// @ts-ignore
+import cors from 'cors'
 const app = express()
 const port = 4000
 
+app.use(cors())
 app.use(express.static('data'))
 
 app.listen(port, () => {
